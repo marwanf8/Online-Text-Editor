ab5ac0f91267d6de323323a915fa6c4d7f2e9fe5
Marwanf8 <marwanfathy86@gmail.com>
2026-09-07 01:42:40 +0300

    Final Script

--- script.js
+++ script.js
@@ -42,3 +42,57 @@ const intializer = () => {
     fontSizeRef.value = 3;
 };
 
+const modifyText = (command, defaultUi, value) => {
+    document.execCommand(command, defaultUi, value);
+};
+
+optionsButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        modifyText(button.id, false, null);
+    });
+
+});
+
+advancedOptionButton.forEach((button) => {
+    button.addEventListener("change", () => {
+        modifyText(button.id, false, button.value);
+    });
+
+});
+
+linkButton.addEventListener("click", () => {
+    let userLink = prompt("Enter a URL?");
+    if(/http/i.test(userLink)){
+        modifyText(linkButton.id. false, userLink);
+    }else {
+        userLink = "http://" + userLink;
+        modifyText(linkButton.id. false, userLink);
+    }
+});
+
+const highlighter = (className, needsRemoval) => {
+    className.forEach((button) => {
+        button.addEventListener("click", () => {
+            if(meedsRemoval){
+                let alreadyActive = false;
+                if(button.classList.contains("active")){
+                    alreadyActive = true;
+                }
+                highlighterRemover(className);
+                if(!alreadyActive){
+                    button.classList.add("active");
+                }
+            }else{
+                button.classList.toggle("active");
+            }
+        });
+    });
+};
+
+const highlighterRemover = (className) => {
+    className.forEach((button) => {
+        button.classList.remove("active");
+    });
+};
+
+window.onload = intializer();
